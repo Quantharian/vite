cbf53782cacdf8b7109636e21f56e73c4a5e5db9
Rikardo Rodríguez <maryxtryx@gmail.com>
2025-01-16 12:33:53 +0100

    Update project

--- demo-pro/vite.config.ts
+++ demo-pro/vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vitest/config';
+import { resolve } from 'path';
 
 export default defineConfig({
     test: {
         globals: true,
     },
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                main1: resolve(__dirname, 'pages/page1.html'),
+                main2: resolve(__dirname, 'page2/index.html'),
+            },
+        },
+    },
+    //
 });
--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,9 +1,18 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vitest/config';
+import { resolve } from 'path';
 
 export default defineConfig({
     test: {
-        include: ['src/**/*.test.ts'],
+        // include: ['**/*.test.ts'],
         globals: true,
+    },
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                about: resolve(__dirname, 'about/index.html'),
+            },
+        },
     },
 });
