a5996a1d28171b0da8103becb285c0ba876a4de6
Rikardo Rodríguez <maryxtryx@gmail.com>
2025-01-21 09:42:41 +0100

    Update with yesterday class

--- demo-js/01-tipos.js
+++ demo-js/01-tipos.js
@@ -74,13 +74,294 @@ console.log(() => {}, typeof (() => {}));
 //  console.log(foo); // ReferenceError: foo is not defined
 
 {
-
     const IVA_VALUES = {
-        base: 1.22,
-        reducido: 1.10,
+        base: 1.21,
+        reducido: 1.1,
         superReducido: 1.04,
-    }
+    };
+
+    Object.freeze(IVA_VALUES);
+
+    // IVA_VALUES.base = 1; TypeError: Cannot assign ...
+
     let price = 22;
     let total = price * IVA_VALUES.base;
-    console.log({total});
+    console.log(total);
+
+    const data = [1, 2, 3];
+}
+{
+    // undefined y null -> nullish
+
+    let foo; //implícito, no intencional -> undefined
+    console.log({ foo });
+    let baz = (() => {})();
+    console.log({ baz });
+
+    // Intencional
+    foo = 'Pepe';
+    foo = null;
+    console.log({ foo });
+    baz = (() => null)();
+    console.log({ baz });
+}
+{
+    // strings
+
+    let user = 'Pepe "El tuerto"';
+    user = "Pepe \n 'El tuerto'";
+
+    console.log(user);
+
+    // template string
+
+    const title = `El tesoro de
+    ${user}`;
+    console.log(title);
+}
+{
+    // numbers
+
+    let n = 1;
+    n = -1;
+    n = 2.4;
+    n = 1_200_300;
+    n = 1.2e6;
+    console.log(n);
+
+    console.log(Number.MAX_VALUE);
+    console.log(Number.MIN_VALUE);
+
+    console.log(Number.MAX_SAFE_INTEGER);
+    console.log(Number.MIN_SAFE_INTEGER);
+
+    n = 9_007_199_254_740_991;
+    console.log(n);
+    console.log(n + 1);
+    console.log(n + 2);
+
+    // BigNumber
+
+    n = 9_007_199_254_740_991n;
+    console.log(n);
+    console.log(n + 1n);
+    console.log(n + 2n);
+}
+{
+    // Números "raritos"
+
+    // cCon bignumber -> lo habitual en programación
+    // const rare = 0n / 0n; // RangeError
+
+    let rare = 0 / 0;
+    console.log(rare, typeof rare); //NaN
+
+    console.log(isNaN(rare)); //true
+    console.log(Number.isNaN(rare)); //true
+
+    // Con conversion de tipos
+    console.log(isNaN('Pepe')); //true
+    // Sin conversion de tipos
+    console.log(Number.isNaN('Pepe')); //false
+
+    // rare = 1n / 0n; // RangeError: Division by zero
+
+    rare = 1 / 0;
+    console.log(rare); // Infinity
+
+    rare = -1 / 0;
+    console.log(rare); // -Infinity
+
+    rare = -0;
+    console.log(rare); // -0
+}
+
+{
+    // casting y coercion
+    // coercion -> implícita
+    // casting -> explícita
+
+    let n = 1;
+    let x = '2';
+
+    let result = n / Number(x); // casting
+    console.log(result);
+
+    // Tipado débil => SIEMPRE hace coerción
+
+    result = n + x; // coerción
+    console.log(result); // 12
+}
+{
+    // truly v. falsy v. nullish
+
+    // falsy -> false
+    console.log(Boolean(false));
+    console.log(Boolean(0));
+    console.log(Boolean(-0));
+    console.log(Boolean(0n));
+    console.log(Boolean(''));
+    console.log(Boolean(NaN));
+    // nullish (??)
+    console.log(Boolean(undefined));
+    console.log(Boolean(null));
+}
+{
+    // Coerción de primitivos a objetos
+    let foo = 'Pepe';
+    // foo.to = 9; // TypeError
+    console.log(foo.toLowerCase());
+}
+{
+    // functions
+    function foo() {} // Declaración
+    const baz = function () {}; // asignación de expresión funcional (anónima)
+    const arrowFoo = () => {}; // asignación de arrow function (anónima)
+
+    // Uso (ejecuto, invoco, run) la función
+    // operador de invocación: ()
+
+    foo();
+    baz();
+    arrowFoo();
+
+    // Son objetos de primera clase
+
+    foo.title = 'Función declarada';
+    baz.title = 'Función por asignación';
+    arrowFoo.title = 'Arrow function';
+
+    console.log(foo, baz, arrowFoo);
+}
+{
+    // objects
+    const obj1 = new Object(); // No se usa
+
+    // Objeto literal (JSON): Douglas Crockford
+    const obj = {
+        name: 'Juan',
+        age: 45,
+        isUser: true,
+    };
+
+    obj1.name = 'Juan';
+    obj1.age = 45;
+    obj1.isUser = true;
+
+    // obj1 = {}; TypeError: Assignment
+    console.log(obj1, obj);
+
+    obj.name = 'Pepe';
+    delete obj.isUser;
+
+    console.log(obj);
+}
+{
+    // Acceso a las propiedades
+
+    const obj = {
+        name: 'Juan',
+        age: 45,
+        isUser: true,
+    };
+
+    // Notación por puntos
+    console.log(obj.age);
+    // console.log(obj['age']);
+
+    // Notación []
+
+    const propertyName = 'name';
+
+    console.log(obj[propertyName]);
+
+    for (const key in obj) {
+        console.log(`La propiedad ${key} vale ${obj[key]}`);
+    }
+}
+{
+    // Arrays
+    const data = [1, 2, 3];
+    const data2 = new Array(1, 2, 3);
+
+    console.log(typeof data, typeof data2);
+
+    data.name = 'Array de números';
+    console.log(data, data2);
+}
+{
+    // Mutabilidad
+
+    // Reasignación de valores INMUTABLES
+    let z = 22;
+    let x = 22;
+    x = 24;
+
+    // const + valor primitivo (INMUTABLE) = CONSTANTE
+    const c = 23;
+    // c = 33; // Error TypeError: Assignment
+
+    // const + objeto -> NO ES CONSTANTE: es MUTABLE
+
+    const obj = {};
+    obj.name = 'Pepe';
+    // obj = {}; // Error TypeError: Assignment
+
+    // const + objeto + Object.freeze = CONSTANTE
+
+    const EMPRESA = { brand: 'CAS', address: '' };
+    Object.freeze(EMPRESA);
+
+    // EMPRESA.brand = 'New Empresa'; // TypeError: Cannot assign to read only
+}
+{
+    //  Métodos (de instancia)
+    const greet = () => 'Hola Mundo';
+    const obj = {
+        greet: function () {
+            return 'Hola Mundo';
+        },
+    };
+
+    console.log(obj.greet());
+    [].at(-1);
+    ''.toLowerCase();
+}
+{
+    // Métodos estáticos (de clase)
+    const obj = {
+        greet: function () {
+            return 'Hola Mundo';
+        },
+    };
+
+    Object.freeze(obj);
+
+    const data = [];
+    console.log(typeof data);
+
+    Array.isArray(data); // true
+
+    Math.random();
+    // new JSON();
+}
+{
+    // Wrapper objects de los primitivos
+
+    const d = '2';
+    console.log(22 / d);
+
+    let foo = 'Hola';
+
+    // const z = new String()
+
+    // Ejemplo de coercion a String
+    console.log(foo.toLowerCase());
+    console.log(foo);
+
+    let n = 22;
+    console.log(n.toFixed(2));
+
+    let big = 2n;
+    big.toString();
 }
--- demo-pro/magnet.md
+++ demo-pro/magnet.md
@@ -15,3 +15,17 @@ magnet:?xt=urn:btih:8AA452D8B9DB41D37E1BBDD4E8DFE7C5AC7CC5E5&dn=EPL_La_impia_osc
 magnet:?xt=urn:btih:553C4B46928D5E124F73FC9D386807A61C9C5406&dn=EPL*El_Pozo_de_la_Ascension*(Ed.\_ilustrada)&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://www.torrent.eu.org:451/announce&tr=udp://exodus.desync.com:6969/announce&tr=udp://p4p.arenabg.ch:1337/announce&tr=udp://tracker.internetwarriors.net:1337/announce
 
 magnet:?xt=urn:btih:229ED4ACCCBEDE03B081220CBA67AD0E6ECEDA3D&dn=EPL*El_Heroe_de_las_Eras*(Ed.\_ilustrada)&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://www.torrent.eu.org:451/announce&tr=udp://exodus.desync.com:6969/announce&tr=udp://p4p.arenabg.ch:1337/announce&tr=udp://tracker.internetwarriors.net:1337/announce
+
+magnet:?xt=urn:btih:C89B9162D7C98E99366DD6184F49819BAD8B3719&dn=EPL_La_campana_Escarlata&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://www.torrent.eu.org:451/announce&tr=udp://exodus.desync.com:6969/announce&tr=udp://p4p.arenabg.ch:1337/announce&tr=udp://tracker.internetwarriors.net:1337/announce
+
+magnet:?xt=urn:btih:CF3BCB8E15EC72A1AEAC266EEC21E594F463F240&dn=EPL_La_republica_de_Otono&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://www.torrent.eu.org:451/announce&tr=udp://exodus.desync.com:6969/announce&tr=udp://p4p.arenabg.ch:1337/announce&tr=udp://tracker.internetwarriors.net:1337/announce
+
+magnet:?xt=urn:btih:96C0FBF1E4090C426699547C5516B73B99EE4727&dn=EPL_Hechicero&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://www.torrent.eu.org:451/announce&tr=udp://exodus.desync.com:6969/announce&tr=udp://p4p.arenabg.ch:1337/announce&tr=udp://tracker.internetwarriors.net:1337/announce
+
+magnet:?xt=urn:btih:F520935645D4DBDA05443468DC24BBA7CE275DA4&dn=EPL_Guerrero&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://www.torrent.eu.org:451/announce&tr=udp://exodus.desync.com:6969/announce&tr=udp://p4p.arenabg.ch:1337/announce&tr=udp://tracker.internetwarriors.net:1337/announce
+
+magnet:?xt=urn:btih:17C20280744FC8030E41DED5D252E6D389F003E9&dn=EPL_La_sombra_de_los_dioses&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://www.torrent.eu.org:451/announce&tr=udp://exodus.desync.com:6969/announce&tr=udp://p4p.arenabg.ch:1337/announce&tr=udp://tracker.internetwarriors.net:1337/announce
+
+magnet:?xt=urn:btih:DD01F0117BA5008B83B9F64267018EDC0158E195&dn=EPL_El_hambre_de_los_dioses&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://www.torrent.eu.org:451/announce&tr=udp://exodus.desync.com:6969/announce&tr=udp://p4p.arenabg.ch:1337/announce&tr=udp://tracker.internetwarriors.net:1337/announce
+
+magnet:?xt=urn:btih:2DD6E7702DFA48D327655517C59C20BC280CE096&dn=EPL*Nacidos_de_la_bruma*(Ed.\_ilustrada)&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://www.torrent.eu.org:451/announce&tr=udp://exodus.desync.com:6969/announce&tr=udp://p4p.arenabg.ch:1337/announce&tr=udp://tracker.internetwarriors.net:1337/announce
